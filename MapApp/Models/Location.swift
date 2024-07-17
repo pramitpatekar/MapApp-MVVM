13c13afeaf2fcd59ba363d6ecc9ed524f144fe97
pramitpatekar <pramitpatekar19@gmail.com>
2024-07-18 00:02:27 +0530

    1

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Ref on 17/07/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
